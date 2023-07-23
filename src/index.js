cf795c45ede672b0b553d558e838fea01d395a80
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-07-23 05:18:07 -0300

    requisito 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const readTalker = require('./utils/readTalker');
+const { readTalker } = require('./utils/readTalker');
 
 const app = express();
 app.use(express.json());
@@ -19,4 +19,18 @@ app.listen(PORT, () => {
 app.get('/talker', async (_req, res) => {
   const readTalkerFile = await readTalker();
   res.status(HTTP_OK_STATUS).send(readTalkerFile);
+});
+
+app.get('/talker/:id', async (req, res) => {
+   // Obtem o ID fornecido na rota e converte para número inteiro
+  const id = parseInt(req.params.id, 10);
+  const rTalkerFile = await readTalker();
+  // Busca a pessoa palestrante no array de talkers pelo ID
+  const talker = rTalkerFile.find((t) => t.id === id);
+
+  if (talker) {
+    res.status(200).json(talker);
+  } else {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
 });
--- src/utils/readTalker.js
+++ src/utils/readTalker.js
@@ -2,14 +2,15 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const readTalker = async () => {
-    try {
-      const data = await fs.readFile(path.resolve(__dirname, '../talker.json', 'utf-8'));
-    
-      return JSON.parse(data);
-    } catch (err) {
-      console.error(`Erro ao ler o arquivo: ${err.message}`);
-    }
-  };
-  module.exports = {
-    readTalker,
-  };
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, '../talker.json'), 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+    throw err; // Adicionando um throw para propagar o erro para quem chama essa função.
+  }
+};
+
+module.exports = {
+  readTalker,
+};
